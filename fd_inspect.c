a509d894ba5b9c5ccba93c1ba9471b73646198e4
dkdougal <ddougal@frontier.com>
2025-09-06 16:00:11 -0600

    Update fd_inspect.c

--- fd_inspect.c
+++ fd_inspect.c
@@ -1,10 +1,15 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main() {
     for (int fd = 0; fd < 10; fd++) {
-        if (fcntl(fd, F_GETFD) != -1)
+        if (fcntl(fd, F_GETFD) != -1) {
             printf("FD %d is open\n", fd);
+        } else {
+            // Optional: print closed descriptors
+            printf("FD %d is closed\n", fd);
+        }
     }
     return 0;
 }
