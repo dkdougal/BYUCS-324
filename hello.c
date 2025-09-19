386ac6bfac827bb6d7a576304b32131d3e56b759
dkdougal <ddougal@frontier.com>
2025-09-19 07:27:43 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,15 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 void doit() {
-  if (fork() == 0) {
-		printf("hello\n");
-		if (fork() == 0)
-		    printf("hello\n");
-		if (fork() > 0)
-		    exit(0);
-}
-	printf(“hello\n”);
+    if (fork() == 0) {
+        printf("hello\n");
+        if (fork() == 0)
+            printf("hello\n");
+        if (fork() > 0)
+            exit(0);
+    }
+    printf("hello\n");
 }
 
 int main() {
-	doit();
-	printf("hello\n");
+    doit();
+    printf("hello\n");
+    return 0;
 }
